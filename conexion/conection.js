e59ed964cb8b5727a14403a87037cea1ebce27dd
CamilosolerB <caansobu2@gmail.com>
2021-10-19 10:06:50 -0500

    v1.2.1

--- conexion/conection.js
+++ conexion/conection.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 module.exports=()=>{
     mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'tienda'
+        host:'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user:'fpfbgg1ykmzbreku',
+        password:'hzmgccsqavc7fhx5',
+        port:"3306",
+        database:'rzh2gn34x6vb0kx0'
     })
 }
