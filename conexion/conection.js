f46d43fdecafbee415834cae7335711dcfa2de01
CamilosolerB <caansobu2@gmail.com>
2021-10-26 15:24:05 -0500

    correcion en la conexion

--- conexion/conection.js
+++ conexion/conection.js
@@ -1,9 +1,20 @@
 const mysql = require('mysql');
-module.exports=()=>{
-    mysql.createConnection({
-        host:'localhost',
+
+    const mysqlconexion = mysql.createConnection({
+        host:'127.0.0.1',
         user:'root',
-        password:'',
+        password:'Camiloesbueno.',
         database:'tienda'
+    });
+
+    mysqlconexion.connect(function(err){
+        if(err){
+            console.log("Error :"+err)
+            return;
+        }
+        else{
+            console.log("Conexion exitosa")
+        }
     })
-}
+
+    module.exports=mysqlconexion;
